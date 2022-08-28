c400272c47b196a912f83ec40d968fe2da8cbd16
davemoz <davejmoz@gmail.com>
2022-08-28 15:50:51 -0400

    Initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+  <header className="header">
+    <div className="branding">
+      Kayla Moz
+    </div>
+    <nav className="navigation">
+
+    </nav>
+  </header>
+}
