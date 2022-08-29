8c0b7ddd21874cd6a9b4250f79b62c22a901831a
davemoz <davejmoz@gmail.com>
2022-08-29 15:03:55 -0400

    Add netlify identity widget

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,26 @@
 import * as React from "react";
+import { Script } from "gatsby";
 import type { HeadFC } from "gatsby";
 
 const IndexPage = () => {
-  return <></>;
+  return (
+    <>
+      <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
+      <Script id="netlify-identity-redirect">
+        {`
+          if (window.netlifyIdentity) {
+            window.netlifyIdentity.on("init", user => {
+              if (!user) {
+                window.netlifyIdentity.on("login", () => {
+                  document.location.href = "/admin/";
+                });
+              }
+            });
+          }
+        `}
+      </Script>
+    </>
+  );
 };
 
 export default IndexPage;
